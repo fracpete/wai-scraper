9f69ce50847722e2a96c85afe016e93a8b325457
Peter Reutemann <fracpete@gmail.com>
2023-12-08 09:55:37 +1300

    removed panopto access; now using find_element(By.ID, ...) calls

--- src/wai/scraper/__init__.py
+++ src/wai/scraper/__init__.py
@@ -1,3 +1,3 @@
 from ._core import logger, init_logger, wait
 from ._driver import init_driver, close_driver, driver_get, requests_get
-from ._login import sso, panopto
+from ._login import sso
--- src/wai/scraper/_login.py
+++ src/wai/scraper/_login.py
@@ -1,48 +1,30 @@
 from wai.scraper import driver_get, wait
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
 def sso(driver, user, pw, delay=10):
     """
-    Performs the single-sign-on login.
+    Performs the single-sign-on login via DUO.
 
     :param driver: the driver instance to use
-    :type driver: webdriver.Firefox
+    :type driver: selenium.webdriver.Firefox
     :param user: the user to use for SSO
     :type user: str
     :param pw: the password to use for SSO
     :type pw: str
+    :param delay: the number of seconds to wait
+    :type delay: int
     """
 
     # gets redirected to login page
-    driver_get(driver, "sso login", "https://www.waikato.ac.nz/cgi-bin/landing/index.cgi")
+    driver_get(driver, "sso login", "https://elearn.waikato.ac.nz/")
 
     # Login page
     # Fill the login form and submit it
-    driver.find_element_by_id('userNameInput').send_keys("waikato.ac.nz\\" + user)
-    driver.find_element_by_id('passwordInput').send_keys(pw)
-    driver.find_element_by_id('submitButton').send_keys(Keys.RETURN)
+    driver.find_element(By.ID, 'userNameInput').send_keys("waikato.ac.nz\\" + user)
+    driver.find_element(By.ID, 'passwordInput').send_keys(pw)
+    driver.find_element(By.ID, 'submitButton').send_keys(Keys.RETURN)
 
     # give 2FA some time to work its magic
     wait("let 2FA finish", delay)
-
-
-def panopto(driver, delay=5):
-    """
-    Performs the login to Panopto.
-
-    :param driver: the driver instance to use
-    :type driver: webdriver.Firefox
-    """
-
-    # log into coursecast
-    # TODO this has changed
-    driver_get(driver, "panopto login", "https://coursecast.its.waikato.ac.nz/Panopto/Pages/Auth/Login.aspx")
-    select = Select(driver.find_element_by_id("providerDropdown"))
-    select.select_by_visible_text('elearn')
-    submit = driver.find_element_by_id("PageContentPlaceholder_loginControl_externalLoginButton")
-    submit.click()
-
-    # give panopto some time
-    wait("let panopto finish", delay)
